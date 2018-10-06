f7c2edf56d3c393690e082a1a669a80621b8f503
geordie-quiroa <gqmail@protonmail.com>
2018-10-06 01:02:12 -0600

    no funciona la db...

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-var express = require('express'), urlHandling= require('./urlWranglingMeths.js'), mongoose = require('mongoose'), bodyParser = require('body-parser'), app = express(), http = require('http');
+var express = require('express'), urlHandling= require('./urlWranglingMeths.js'), 
+bodyParser = require('body-parser'), app = express(), http = require('http');
 app.use(bodyParser.urlencoded({extended:true})); // esto es para parsear el body & middleware
 app.use(bodyParser.json()); //midleware 
+var  dbConfig = require('./configs/mongoDB.config.js'), mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
 //var charsAvailable= chars.length;
 app.get("/", (req, res)=>{
     res.send(urlHandling.getTinyUrl("frag.me/GNktcm"));
