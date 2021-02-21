7744b4c0fefb6b6ab48c027bcd1f98c666b19d25
Geordie Quiroa <geordie@Geordies-MacBook-Air.local>
2021-02-21 14:19:10 -0600

    Enable cors and docker with ansible

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ app.use(function (req, res, next) {
   //res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:62856')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Credentials', 'false')
   next()
 });
 app.use(express.static(__dirname + '/views'));
